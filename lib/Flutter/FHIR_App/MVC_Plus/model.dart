d557e73ccc07d320df120cf72e55a26abb906855
Marco Prenassi <m.prenassi@gmail.com>
2025-05-21 16:43:55 +0200

    prova commit nuovo branch

--- lib/Flutter/FHIR_App/MVC_Plus/model.dart
+++ lib/Flutter/FHIR_App/MVC_Plus/model.dart
@@ -10,18 +10,17 @@ class FhirObservation {
     required this.value,
     required this.unit,
   });
+  // NEW COMMENT
 
   factory FhirObservation.fromEntry(Map<String, dynamic> entry) {
     final resource = entry['resource'] ?? {};
     // FIXME: Resolve this error
-    final code = resource['code']?['text'] ?? 'Unknown';
+    // final code = resource['code']?['text'] ?? 'Unknown';
     // TODO: Check this solutions, let's learn how to use the debugger
-    //final code = resource['code']['coding'][0] ? ['code'] ?? 'Unknown'; // FAST BUT TOO STRICT
-    //final code = resource['code'].toString(); // FAST BUT TOO GENERAL
-    /* JUST RIGHT ... FOR TWO LIMITED CASES
+    // final code = resource['code']['coding'][0] ? ['code'] ?? 'Unknown'; // FAST BUT TOO STRICT
+    // final code = resource['code'].toString(); // FAST BUT TOO GENERAL
+    // JUST RIGHT ... FOR TWO LIMITED CASES
     final code;
-    var debug = resource['code'].containsKey('coding');
-
     if(resource['code'].containsKey('coding')) {
       code = resource['code']['coding'][0] ? ['code'] ?? 'Unknown';
     }
@@ -29,7 +28,7 @@ class FhirObservation {
       code = resource['code'] ? ['text'] ?? 'Unknown';
     } else
       code = 'Unknown';
-     */
+
 
 
 
